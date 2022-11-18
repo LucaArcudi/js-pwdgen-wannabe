0d17ddad7de3a5b5750711a679c3ed7c0700dbc9
LucaArcudi <arcudilu@gmail.com>
2022-11-18 20:24:39 +0100

    implemented js

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let nome = prompt('Inserisci il tuo nome');
+let cognome = prompt('Inserisci il tuo cognome');
+let colorePreferito = prompt('Inserisci il tuo colore preferito');
+document.getElementById('output').innerHTML = (`${nome} ${cognome} ${colorePreferito} ${22}`);
+
+
+
+
